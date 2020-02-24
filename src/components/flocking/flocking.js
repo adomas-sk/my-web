103fdcc493d348d53be21564c673862c49d2c2f6
Adomas Skliutas <adomas.skliutas@instarem.com>
2020-02-25 00:40:41 +0200

    adjusted variables

--- src/components/flocking/flocking.js
+++ src/components/flocking/flocking.js
@@ -9,23 +9,26 @@ let graphics;
 scene.preload = function() {
 	graphics = scene.add.graphics({
 		lineStyle: { width: 2, color: 0xff0000 },
-		fillStyle: { color: 0x5b3cc9 },
+		// fillStyle: { color: 0x5b3cc9 },
+		fillStyle: { color: 0x5555ff },
 	});
 };
 
 const flock = [];
 
 scene.create = function() {
-	[...Array(50).keys()].forEach(() => {
+	[...Array(100).keys()].forEach(() => {
     const position = new Phaser.Math.Vector2(Phaser.Math.Between(0, scene.game.canvas.width), Phaser.Math.Between(0, scene.game.canvas.height));
 		flock.push(new Boid(position, graphics));
 	});
 };
 
 scene.update = function() {
-	graphics.clear();
-	flock.forEach(boid => {
+  graphics.clear();
+  flock.forEach(boid => {
     boid.run(flock);
+	});
+	flock.forEach(boid => {
 		boid.update();
 		boid.show();
 	});
